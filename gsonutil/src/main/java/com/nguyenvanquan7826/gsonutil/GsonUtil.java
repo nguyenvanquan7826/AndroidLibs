efd64dfe5ae783c867335865cc749e0f6377996d
nguyenvanquan7826 <nguyenvanquan7826@gmail.com>
2019-10-31 18:07:11 +0700

    add GsonUtil, TextViewTwoLineSwitchProgress, hindKeyBroad, fix pageket name

--- gsonutil/src/main/java/com/nguyenvanquan7826/gsonutil/GsonUtil.java
+++ gsonutil/src/main/java/com/nguyenvanquan7826/gsonutil/GsonUtil.java
@@ -1,4 +1,4 @@
-package com.doanthidiem.gsonutil;
+package com.nguyenvanquan7826.gsonutil;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
--- gsonutil/src/test/java/com/nguyenvanquan7826/gsonutil/ExampleUnitTest.java
+++ gsonutil/src/test/java/com/nguyenvanquan7826/gsonutil/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.doanthidiem.gsonutil;
+package com.nguyenvanquan7826.gsonutil;
 
 import org.junit.Test;
 
