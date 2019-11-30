56c6954e4a09356df5a8aefb38f506fb8cf3e8ae
nguyenvanquan7826 <nguyenvanquan7826@gmail.com>
2019-11-30 17:02:34 +0700

    change lib version appcopat and material

---
+++ appbase/src/main/java/com/nguyenvanquan7826/appbase/fragment/BaseTabFragment.java
@@ -0,0 +1,84 @@
+package com.nguyenvanquan7826.appbase.fragment;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.tabs.TabLayout;
+import com.nguyenvanquan7826.appbase.PagerAdapter;
+import com.nguyenvanquan7826.appbase.R;
+import com.nguyenvanquan7826.appbase.activity.BaseContainFragmentActivity;
+
+import java.util.Objects;
+
+public class BaseTabFragment extends InAcFragment {
+
+    private PagerAdapter<Fragment> pagerAdapter;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.base_tab_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        final ViewPager pager = findViewById(R.id.pager);
+        TabLayout tabLayout = findViewById(R.id.tab);
+
+        pagerAdapter = new PagerAdapter<>(getChildFragmentManager());
+        pager.setAdapter(pagerAdapter);
+        pager.setOffscreenPageLimit(pagerAdapter.getCount());
+        tabLayout.setupWithViewPager(pager);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            tabLayout.setElevation(8);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        hindElevationAppBar();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        showElevationAppBar();
+    }
+
+    protected AppBarLayout getAppBarLayout() {
+        return ((BaseContainFragmentActivity) Objects.requireNonNull(getActivity())).getAppBarLayout();
+    }
+
+    protected void hindElevationAppBar() {
+        getAppBarLayout().setElevation(0);
+    }
+
+    protected void showElevationAppBar() {
+        getAppBarLayout().setElevation(8);
+    }
+
+    protected void addTab(Fragment fragment, String title) {
+        pagerAdapter.add(fragment, title);
+    }
+
+    protected void build() {
+        pagerAdapter.notifyDataSetChanged();
+    }
+}
